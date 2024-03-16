d786b7ae89bf4889bb4041a16e469fe33c85bff6
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-03-16 18:05:11 -0300

    Features 5,6,7,8,9 finalizadas
    finalizadas Features 5,6,7,8,9

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,62 @@ lucas.follow(kadu)
 kadu.follow(kadu) // nao vai parecer na lista
 kadu.showfollowing()
 console.log("---------------------------------")
+
+// 5 - Deve ser possível para um usuário curtir os tweets de
+// outros usuários.
+// Um tweet (normal ou reply) pode ter 0 ou N curtidas.
+
+kadu.likeTweet(newTweetKadu)
+maria.likeTweet(newTweetKadu)
+maria.likeTweet(newTweetKadu)
+console.log("---------------------------------")
+
+// 6 - Deve ser possível para um usuário responder a tweets
+// de outros usuários.
+// A resposta a um tweet deve ser um novo tweet com o
+// tipo “reply”.
+
+newTweetKadu.reply(maria,"reply de tweet teste")
+newTweetKadu.reply(maria,"TESTANDO REPLLLLLLLLLLLLLLY")
+console.log("---------------------------------")
+
+// 7 - Deve ser possível exibir tweets com a informação de
+// seus likes recebidos.
+
+// @<username>: <conteúdo>
+
+// <likes>
+
+// A exibição dos likes varia dependendo da quantidade:
+// ● Sem like - não exibir.
+// ● 1 like – “@<username> curtiu”.
+// ● 2 ou + likes – “@<username1> e mais X
+// usuários curtiram”.
+
+kadu.showTweets()
+console.log("---------------------------------")
+
+// 8 - Deve ser possível exibir tweets com os seus replies.
+
+// @<username>: <conteúdo>
+
+// <replies>
+
+// Adicione um indicador ao início do reply para indicar
+// que se trata de uma resposta (p.ex. o caractere “>”).
+
+// @username: conteúdo do tweet original.
+// > @username2: conteúdo do reply 1.
+// > @username3: conteúdo do reply 2.
+console.log("---------------------------------")
+newTweetKadu.showReplies()
+
+// 9 - Um usuário poderá exibir o seu feed de tweets. O feed
+// é composto por seus tweets e pelos tweets dos
+// usuários a quem segue.
+// Na exibição do feed, cada tweet deverá ser exibido
+// conforme features anteriores: com seu conteúdo, seus
+// likes e replies.
+console.log("---------------------------------")
+
+kadu.showFeed()
